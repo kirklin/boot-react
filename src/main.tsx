e1eb25f8f0cf1627f13ee9a22ab3ead202d8986e
Kirk Lin <linkirk@163.com>
2023-08-14 15:38:33 +0800

    feat: support `unocss`

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// reset css
+import "@kirklin/reset-css/kirklin.css";
+import "~/styles/main.css";
+import "uno.css";
+
+ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import UnoCss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 8888,
+    open: true,
+    https: false,
+    proxy: {},
+  },
+  plugins: [
+    react(),
+    // https://github.com/unocss/unocss
+    // see unocss.config.ts for config
+    UnoCss(
+      {
+        configFile: resolve(__dirname, "src/config/unocss/index.ts"),
+      },
+    ),
+  ],
+  resolve: {
+    alias: {
+      "~/": `${resolve(__dirname, "src")}/`,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+      @import "~/styles/variables.scss";
+    `,
+        javascriptEnabled: true,
+      },
+    },
+  },
 });
